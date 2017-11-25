3fce7b946f1e3ece0c453e735dc39f9feecd0231
blumlblum <yiqiaojiang@outlook.com>
2017-11-25 22:22:46 +0800

    vertify the images name

--- result_Window.py
+++ result_Window.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'add_moreMessage.ui'
-#
+
 # Created by: PyQt5 UI code generator 5.6
 #
 # WARNING! All changes made in this file will be lost!
